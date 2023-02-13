e7bde7803070267e5c815731926a4fdf13bb9cac
zhengzhouyi2020 <zhengzhouyi_2020@qq.com>
2023-02-13 20:32:29 +0800

    RPC Service init commit

---
+++ netty-rpc-client/src/main/java/com/zzy/rpc/connect/ConnectManager.java
@@ -0,0 +1,9 @@
+package com.zzy.rpc.connect;
+
+/**
+ * @Author:zzy
+ * @Date:2023/2/13
+ * @Description: RPC Connection Manager
+ */
+public class ConnectManager {
+}
---
+++ netty-rpc-client/src/main/java/com/zzy/rpc/discovery/ServiceDiscovery.java
@@ -0,0 +1,9 @@
+package com.zzy.rpc.discovery;
+
+/**
+ * @Author:zzy
+ * @Date:2023/2/13
+ * @Description: 服务发现
+ */
+public class ServiceDiscovery {
+}
