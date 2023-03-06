9aa07840befc81fd512da9f852842cc8d040bd4e
zhengzhouyi2020 <zhengzhouyi_2020@qq.com>
2023-03-06 19:00:20 +0800

    fix some bugs

--- netty-rpc-test/src/main/java/com/zzy/netty/test/server/RpcServerBootstrap2.java
+++ netty-rpc-test/src/main/java/com/zzy/netty/test/server/RpcServerBootstrap2.java
@@ -10,7 +10,7 @@ public class RpcServerBootstrap2 {
     private static final Logger logger = LoggerFactory.getLogger(RpcServerBootstrap2.class);
 
     public static void main(String[] args) {
-        String serverAddress = "127.0.0.1:18877";
+        String serverAddress = "127.0.0.1:2181";
         String registryAddress = "10.217.59.164:2181";
         RpcServer rpcServer = new RpcServer(serverAddress, registryAddress);
         HelloService helloService1 = new HelloServiceImpl();
